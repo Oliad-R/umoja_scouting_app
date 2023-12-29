718e9e46a831dffbfc61720db55e99854de3aaea
Oliad Rundassa <runol@utschools.ca>
2023-12-29 15:30:36 -0500

    pin utility implemented. card ui finished

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,13 @@
 "use client";
 
-import { useState, useEffect } from 'react';
+import { useState, useEffect, SetStateAction } from 'react';
 import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from '@/components/ui/card';
 import {  Select,  SelectContent,  SelectGroup,  SelectItem,  SelectLabel,  SelectTrigger,  SelectValue } from "@/components/ui/select"
-import { RotateCw, Pencil, Eye, Pin } from 'lucide-react';
+import { RotateCw, Pencil, Eye, Pin, PinOff, RefreshCcw, Loader2 } from 'lucide-react';
 import { Input } from '@/components/ui/input';
 import { Button } from '@/components/ui/button';
 import Loading from './loading'; // Adjust the import path as needed
+import { Value } from '@radix-ui/react-select';
 
 interface Team {
   team_number: number;
@@ -19,6 +20,8 @@ export default function Home() {
   const [data, setData] = useState<Team[]>([]);
   const [searchInput, setSearchInput] = useState('');
   const [isLoading, setLoading] = useState(false);
+  const [filterVal, setFilter] = useState('');
+  const [pinnedTeams, setPinnedTeams] = useState({});
   const apiUrl = 'https://www.thebluealliance.com/api/v3/district/2023ont/teams';
 
   async function pullJson() {
@@ -40,7 +43,6 @@ export default function Home() {
 
   useEffect(() => {
     pullJson();
-    // console.log(pullJson())
   }, []);
 
   const handleSearchChange = (e) => {
@@ -48,53 +50,81 @@ export default function Home() {
   }
 
   const filteredData = data.filter(team =>
-    team.nickname.trim().toLowerCase().includes(searchInput.trim().toLowerCase()) || team.team_number.toString().includes(searchInput.trim().toLowerCase())
+    pinnedTeams[team.team_number] || team.nickname.trim().toLowerCase().includes(searchInput.trim().toLowerCase()) || team.team_number.toString().includes(searchInput.trim().toLowerCase())
   );
+
+  const sortedData = filteredData.sort((a,b) => {
+    if (pinnedTeams[a.team_number] && !pinnedTeams[b.team_number]){
+      return -1;
+    }
+    if (!pinnedTeams[a.team_number] && pinnedTeams[b.team_number]){
+      return 1;
+    }
+    return 0;
+  });
+
+  const handleFilterChange = (e) => {
+    setFilter(e);
+    console.log("Filter:"+filterVal);
+  }
 
   function refreshSearch() {
     setSearchInput('');
+    setFilter('');
+    setPinnedTeams({});
     pullJson();
   }
+
+  const togglePinnedTeam = (teamNumber) => {
+    setPinnedTeams(prev => ({
+      ...prev,
+      [teamNumber]: !prev[teamNumber]
+    }));
+  };
+
+  function pinIcon(teamNumber: number) {return pinnedTeams[teamNumber] ? <PinOff/> : <Pin/>}
+
+  function refreshIcon() {return isLoading ? <Loader2 className="animate-spin"/>: <RotateCw/>}
 
   return (
     <main>
       <div className="flex w-full items-center space-x-2"> 
       {/* Should probably be a multiselect */}
             <Input type="text" placeholder='Search' value={searchInput} onChange={handleSearchChange}/>
-            <Select>
+            <Select value={filterVal} onValueChange={handleFilterChange}>
               <SelectTrigger className="w-[280px]">
-                <SelectValue placeholder="Filter" />
+                <SelectValue placeholder="Filter"/>
               </SelectTrigger>
               <SelectContent>
                 <SelectGroup>
                   <SelectLabel>Drive Train</SelectLabel>
-                  <SelectItem value="est">Tank Drive</SelectItem>
-                  <SelectItem value="cst">Swerve Drive</SelectItem>
-                  <SelectItem value="mst">Mecanum Drive</SelectItem>
-                  <SelectItem value="pst">West-Coast Drive</SelectItem>
+                  <SelectItem value="td">Tank Drive</SelectItem>
+                  <SelectItem value="sd">Swerve Drive</SelectItem>
+                  <SelectItem value="md">Mecanum Drive</SelectItem>
+                  <SelectItem value="wcd">West-Coast Drive</SelectItem>
                 </SelectGroup>
                 <SelectGroup>
                   <SelectLabel>Intake</SelectLabel>
-                  <SelectItem value="gmt">Low</SelectItem>
-                  <SelectItem value="cet">Mid</SelectItem>
-                  <SelectItem value="eet">High</SelectItem>
+                  <SelectItem value="li">Low</SelectItem>
+                  <SelectItem value="mi">Mid</SelectItem>
+                  <SelectItem value="hi">High</SelectItem>
                 </SelectGroup>
                 <SelectGroup>
                   <SelectLabel>Autonomous</SelectLabel>
-                  <SelectItem value="msk">No Auto</SelectItem>
-                  <SelectItem value="ist">Working Auto</SelectItem>
-                  <SelectItem value="cst_china">Advanced Auto</SelectItem>
+                  <SelectItem value="na">No Auto</SelectItem>
+                  <SelectItem value="wa">Working Auto</SelectItem>
+                  <SelectItem value="aa">Advanced Auto</SelectItem>
                 </SelectGroup>
               </SelectContent>
             </Select>
-            <Button onClick={refreshSearch} type="button"><RotateCw/></Button>
+            <Button onClick={refreshSearch} variant="secondary" type="button">{refreshIcon()}</Button>
       </div>
       {isLoading ? (
-        <Loading /> // Render the loading component while data is being fetched
+        <Loading />
       ) : (
         <>
           <div className='grid md:grid-cols-3 grid-cols-1 gap-8'>
-            {filteredData.map(team => (
+            {sortedData.map(team => (
               <Card key={team.team_number}>
                 <CardHeader>
                   <div>
@@ -105,7 +135,7 @@ export default function Home() {
                 <CardFooter className="space-x-3">
                   <Button><Eye/></Button>
                   <Button><Pencil/></Button>
-                  <Button><Pin/></Button>
+                  <Button onClick={() => togglePinnedTeam(team.team_number)}>{pinIcon(team.team_number)}</Button>
                 </CardFooter>
               </Card>
             ))}
--- src/components/SkeletonCard.tsx
+++ src/components/SkeletonCard.tsx
@@ -1,20 +1,20 @@
 import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from '@/components/ui/card';
 import { Skeleton } from './ui/skeleton';
+import { Button } from './ui/button';
 
 export default function SkeletonCard(){
     return (
         <Card className="flex flex-col justify-between">
-            <CardHeader className="flex-row gap-4 items-center">
-                <Skeleton className="h-12 flex-grow"/>
+            <CardHeader className="flex-row gap-4 items-center pb-0">
+                <Skeleton className="h-7 flex-grow"/>
             </CardHeader>
             <CardDescription>
-                <Skeleton className="h-12 flex-grow"/>
+                <Skeleton className="h-4 w-1/2 mx-6 mb-5 flex-grow"/>
             </CardDescription>
-            {/* <CardContent>
-                <Skeleton className="h-12 flex-grow"/>
-            </CardContent> */}
-            <CardFooter>
-                <Skeleton className="h-12 flex-grow"/>
+            <CardFooter className="space-x-3 mb-5">
+                <Skeleton className="h-10 w-10"/>
+                <Skeleton className="h-10 w-10"/>
+                <Skeleton className="h-10 w-10"/>
             </CardFooter>
         </Card>
     )
